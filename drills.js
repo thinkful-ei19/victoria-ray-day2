ae33c4f7406a91e9f791dee96a54183622ca8d72
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-02-20 13:10:03 -0800

    added hazardWarningCreator function

--- drills.js
+++ drills.js
@@ -57,22 +57,49 @@ console.log(min([3, 5, 7, 9, 12])); */
 
 
 
-// Return only names that begin with 'R'
-const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+// // Return only names that begin with 'R'
+// const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
-const filteredNames = filter(myNames, function(name) {
-    // This is a "predicate function" - it's a function that only returns a boolean
-    return name[0] === 'R';
-});
+// const filteredNames = filter(myNames, function(name) {
+//     // This is a "predicate function" - it's a function that only returns a boolean
+//     return name[0] === 'R';
+// });
 
-console.log(filteredNames) // => ['Rich', 'Ray']
+// console.log(filteredNames) // => ['Rich', 'Ray']
 
-function filter(arr, fn) {
-  var newArray = [];
-  for (var i = 0; i < arr.length; i++){
-    if (fn(arr[i]) === true){
-      newArray.push(arr[i]);
+// function filter(arr, fn) {
+//   var newArray = [];
+//   for (var i = 0; i < arr.length; i++){
+//     if (fn(arr[i]) === true){
+//       newArray.push(arr[i]);
+//     }
+//   }
+//   return newArray;
+// }
+
+
+function hazardWarningCreator(typeOfWarning) {
+    let warningCounter = 0;
+    return function(location) {
+        warningCounter++;
+        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
     }
-  }
-  return newArray;
 }
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const iceWarning = hazardWarningCreator('Icy roads');
+const robotWarning = hazardWarningCreator('Terminators on the Road');
+
+rocksWarning('Main St and Pacific Ave');
+// => DANGER There is a Rocks on the Road hazard at Main St and Pacific Ave
+// => The Rocks on the Road hazard has triggered 1 time(s) today!
+
+rocksWarning('Centinela Ave and Olympic Blvd');
+// => DANGER There is a Rocks on the Road hazard at Centinela Ave and Olympic Blvd
+// => The Rocks on the Road hazard has triggered 2 time(s) today!
+
+// Invoke the other hazard creators you instantiated, too
+
+robotWarning('Seattle Ave and London Blvd');
+
