d2234e18083f0d7807ae02dce06a55ae79f8544a
Victoria Daug <victoria.daug@gmail.com>
2018-02-20 22:22:34 +0000

    forEach, filter and map

--- drills.js
+++ drills.js
@@ -78,28 +78,39 @@ console.log(min([3, 5, 7, 9, 12])); */
 // }
 
 
-function hazardWarningCreator(typeOfWarning) {
-    let warningCounter = 0;
-    return function(location) {
-        warningCounter++;
-        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
-        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
-    }
-}
+// function hazardWarningCreator(typeOfWarning) {
+//     let warningCounter = 0;
+//     return function(location) {
+//         warningCounter++;
+//         console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+//         console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+//     }
+// }
+//
+// const rocksWarning = hazardWarningCreator('Rocks on the Road');
+// const iceWarning = hazardWarningCreator('Icy roads');
+// const robotWarning = hazardWarningCreator('Terminators on the Road');
+//
+// rocksWarning('Main St and Pacific Ave');
+// // => DANGER There is a Rocks on the Road hazard at Main St and Pacific Ave
+// // => The Rocks on the Road hazard has triggered 1 time(s) today!
+//
+// rocksWarning('Centinela Ave and Olympic Blvd');
+// // => DANGER There is a Rocks on the Road hazard at Centinela Ave and Olympic Blvd
+// // => The Rocks on the Road hazard has triggered 2 time(s) today!
+//
+// // Invoke the other hazard creators you instantiated, too
+//
+// robotWarning('Seattle Ave and London Blvd');
 
-const rocksWarning = hazardWarningCreator('Rocks on the Road');
-const iceWarning = hazardWarningCreator('Icy roads');
-const robotWarning = hazardWarningCreator('Terminators on the Road');
+function turtleSteps(arr){
+  arr.filter(function(steps) {
+    return steps[0] >= 0 && steps[1] >= 0
+  }).map(function(val) {
+    return val[0] + val[1]
+  }).forEach(function(val) {
+    console.log(`Steps Taken: ${val}`)
+  })
+  }
 
-rocksWarning('Main St and Pacific Ave');
-// => DANGER There is a Rocks on the Road hazard at Main St and Pacific Ave
-// => The Rocks on the Road hazard has triggered 1 time(s) today!
-
-rocksWarning('Centinela Ave and Olympic Blvd');
-// => DANGER There is a Rocks on the Road hazard at Centinela Ave and Olympic Blvd
-// => The Rocks on the Road hazard has triggered 2 time(s) today!
-
-// Invoke the other hazard creators you instantiated, too
-
-robotWarning('Seattle Ave and London Blvd');
-
+console.log(turtleSteps([[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]]))
